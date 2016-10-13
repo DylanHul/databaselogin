feafa34ab1b95ff51bcf0c811853b16abb07008b
DylanHul <dylan.hulain@gmail.com>
2016-10-13 23:48:52 +1300

    users table find with knex db

--- database/db.js
+++ database/db.js
@@ -1,2 +1,10 @@
 const config      = require('../../knexfile').development
 const knex        = require('knex')(config)
+
+const findUserByName = (user_name) => {
+  return knex('users')
+    .where('user_name', user_name)
+    .then( logInData => {
+      return logInData
+    })
+}
