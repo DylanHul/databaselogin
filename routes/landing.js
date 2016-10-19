c446df136df345ca4489881b5039be799c75ee19
DylanHul <dylan.hulain@gmail.com>
2016-10-19 22:22:56 +1300

    created landing page

--- routes/landing.js
+++ routes/landing.js
@@ -2,3 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const {findUserByName, getUser}  = require('../database/db');
+
+router.get('/', (req, res) => {
+  res.send('Hello and Welcome')
+})
+
+module.exports = router;
