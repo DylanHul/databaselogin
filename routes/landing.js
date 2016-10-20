621f7bb1af773162478e58607afb85c7d9363897
DylanHul <dylan.hulain@gmail.com>
2016-10-20 20:49:21 +1300

    created landing hbs

--- routes/landing.js
+++ routes/landing.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {findUserByName, getUser}  = require('../database/db');
 
 router.get('/', (req, res) => {
-  res.send('Hello and Welcome')
+  res.render('landing')
 })
 
 module.exports = router;
