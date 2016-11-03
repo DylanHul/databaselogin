a8056b93c27a2f40f88db4dc21f30f25f571b6b8
DylanHul <dylan.hulain@gmail.com>
2016-11-03 23:57:37 +1300

    action created

--- client/action/action.js
+++ client/action/action.js
@@ -1,1 +1,24 @@
 import React from 'react'
+
+//account meaning the users (user account)
+
+function getAccountById (id) {
+ return (dispatch) => {
+   request
+   .get(`/api/v1/users/${id}`)
+   .end((err, res) => {
+       if (err) {
+         console.error(err.message)
+         return
+       }
+       dispatch(receiveAccountId(JSON.parse(res.text)))
+     })
+ }
+}
+
+const receiveAccountId = (payload) => {
+  return {
+    type: 'RECEIVE_ACCOUNT_ID',
+    payload: payload
+  }
+}
