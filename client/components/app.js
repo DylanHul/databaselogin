fb888b6c1b31f4476af24b1a7757370f82947e8b
DylanHul <dylan.hulain@gmail.com>
2016-11-15 22:12:02 +1300

    adding components

---
+++ client/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Transaction from './Transaction'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { getAccountById } from '../actions/action'
+
+const mapStateToProps = (state) => {
+  return {
+
+
+render() {
+
+
+}
